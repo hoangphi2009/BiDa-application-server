d5f07e35c4b3f5b7d68f60630e99f7ee28e41d8e
LamVC4869 <lamcity4869@gmail.com>
2025-07-10 00:31:25 +0700

    feature-BD add new function getAllTables

--- routes/table.route.js
+++ routes/table.route.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import { createTable } from '../controllers/tableController/createTable.controller.js';
 import { deletedTable } from '../controllers/tableController/deleteTable.controller.js';
+import { getAllTables } from '../controllers/tableController/getAllTable.controller.js';
 
 const router = express.Router();
 router.post('/register', createTable);
 router.delete('/delete/:tableId', deletedTable);
+router.get('/tables', getAllTables);
 export default router;
