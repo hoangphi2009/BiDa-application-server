ac24498b4ca6980b58fe4ed41d7ebfdf1289eb9b
LamVC4869 <lamcity4869@gmail.com>
2025-07-10 22:09:47 +0700

    create function get table by id

--- routes/table.route.js
+++ routes/table.route.js
@@ -2,9 +2,11 @@ import express from 'express';
 import { createTable } from '../controllers/tableController/createTable.controller.js';
 import { deletedTable } from '../controllers/tableController/deleteTable.controller.js';
 import { getAllTables } from '../controllers/tableController/getAllTable.controller.js';
+import { getTableById } from '../controllers/tableController/getTableById.controller.js';
 
 const router = express.Router();
 router.post('/register', createTable);
 router.delete('/delete/:tableId', deletedTable);
 router.get('/tables', getAllTables);
+router.get('/tables/:tableId', getTableById);
 export default router;
